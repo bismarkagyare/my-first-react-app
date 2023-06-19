454b1fe34b8224307bd2ff698201b18d5a3a06e9
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-19 22:50:37 +0000

    create more squares

--- src/square.js
+++ src/square.js
@@ -2,7 +2,19 @@
 import React from 'react';
 
 const Square = () => {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 };
 
 export default Square;

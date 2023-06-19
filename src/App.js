bea6c1fdf4efe12469ab637b39a9830da658653a
Bismark Agyare <bismarkagyare54@gmail.com>
2023-06-19 22:55:09 +0000

    comment out the header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Square from './square';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +18,8 @@ function App() {
         >
           Learn React
         </a>
-        <Square />
-      </header>
+      </header> */}
+      <Square />
     </div>
   );
 }

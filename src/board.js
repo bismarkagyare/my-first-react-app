7c25387f568b3b6d258cd814ac4a29884e378d80
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-01 20:49:33 +0000

    implement handleClick function in board component

--- src/board.js
+++ src/board.js
@@ -12,10 +12,17 @@ const Square = ({ value, onSquareClick }) => {
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = () => {
+    const nextSquares = squares.slice();
+    nextSquares[0] = 'X';
+    setSquares(nextSquares);
+  };
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>

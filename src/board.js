66f0825fa8894f436841074193cbe657f6d08d80
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-02 16:22:21 +0000

    implement turn based logic

--- src/board.js
+++ src/board.js
@@ -11,12 +11,20 @@ const Square = ({ value, onSquareClick }) => {
 };
 
 const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
+    if (squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
 
   return (
